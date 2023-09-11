29560dd30c3571fcb29b0f66a833672d650097a0
TEOC <Foox210@yandex.ru>
2023-09-11 19:30:44 +0400

    first commit

--- src/Client.java
+++ src/Client.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class MainClient {
+public class Client {
     public static void main(String[] args) {
         String host = "localhost";
         int port = 8080;
